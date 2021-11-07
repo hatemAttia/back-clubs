274e0e5fce141188d4af749696d1f3775732c74f
bedis <hanzoutib@gmail.com>
2021-11-07 23:52:52 +0100

    multiple delete

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,13 @@ export default (app) => {
 
     app.route("/admin/register").post(adminController.register);
     app.route("/admin").post(adminController.login);
+    app.route("/admin/club").post(clubController.createClub);
+    app.route("/admin/club").get(clubController.getAllClubs);
+    app.route("/admin/club/:id").delete(clubController.deleteClub);
+    app.route("/admin/clubs").post(clubController.deleteMultipleClub);
+
+    app.route("/admin/club/:id").put(clubController.updateClub);
+    app.route("/admin/club-img/:id").post(uploadimages.single("file"), clubController.updateImage);
 
     ////////////////////////Category CONTROLLER///////////////////////////
     app.route("/category").post(categoryController.createCategory);
